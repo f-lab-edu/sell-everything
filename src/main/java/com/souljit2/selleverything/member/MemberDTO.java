f5c2599ed4ce97b1feb54d8bc8f1b2820b934e57
wooyoung <souljit2@gmail.com>
2021-04-22 11:36:03 +0900

    refactor: memberNo 타입 변경 및 불필요한 주석 제거
    memberNo를 primitive type으로 변경하고, 필요하지 않은 주석을 제거

--- src/main/java/com/souljit2/selleverything/member/MemberDTO.java
+++ src/main/java/com/souljit2/selleverything/member/MemberDTO.java
@@ -8,14 +8,14 @@ import java.sql.Timestamp;
 @ToString
 @EqualsAndHashCode
 public class MemberDTO {
-    private Integer memberNo;                   /* 사용자 no(pk)*/
-    private String memberId;                    /* 사용자 id */
-    private String memberPassword;              /* 사용자 password */
-    private String memberName;                  /* 사용자 이름 */
-    private String memberNickname;              /* 사용자 닉네임 */
-    private String memberPhone;                 /* 사용자 휴대폰 번호*/
+    private int memberNo;                       /* 사용자 pk */
+    private String memberId;
+    private String memberPassword;
+    private String memberName;
+    private String memberNickname;
+    private String memberPhone;
     private Boolean membershipAgreementYn;      /* 개인정보 활용 약관 동의*/
     private Boolean optionalInfoAgreementYn;    /* 선택정보 활용 약관 동의 */
-    private Timestamp createdAt;                /* 사용자 정보 생성 일시 */
+    private Timestamp createdAt;
     private Timestamp updatedAt;                /* 사용자 정보 수정 일시 */
 }
