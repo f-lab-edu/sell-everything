7f634d415972645d1041a61aba780bde4ee3e586
wooyoung <souljit2@gmail.com>
2021-04-24 16:16:35 +0900

    feat: 회원가입 요청을 처리할 MemberController 구현
    회원가입 성공시 200 ok 메시지 전송
    아이디 중복시 409 conflict 메시지 전송

---
+++ src/main/java/com/souljit2/selleverything/member/MemberController.java
@@ -0,0 +1,31 @@
+package com.souljit2.selleverything.member;
+
+import com.souljit2.selleverything.messages.ApiErrorMessage;
+import com.souljit2.selleverything.messages.ApiResponseMessage;
+import com.souljit2.selleverything.messages.ApiSuccessMessage;
+import lombok.AllArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/apis/auth")
+public class MemberController {
+
+    private MemberServiceImpl memberService; /* @AllArgsConstructor에 의해 주입 */
+    @PostMapping("/signUp")
+    public ResponseEntity<ApiResponseMessage> signUp(@RequestBody @Valid MemberDTO memberDTO) throws Exception {
+        try {
+            memberService.signUp(memberDTO);
+            ApiResponseMessage message = new ApiSuccessMessage<MemberDTO>("회원가입", "회원가입 성공", null);
+            return new ResponseEntity<ApiResponseMessage>(message, HttpStatus.OK);
+        } catch (DuplicateKeyException e){
+            ApiResponseMessage message = new ApiErrorMessage("회원가입", "아이디 중복");
+            return new ResponseEntity<ApiResponseMessage>(message, HttpStatus.CONFLICT);
+        }
+    }
+}
