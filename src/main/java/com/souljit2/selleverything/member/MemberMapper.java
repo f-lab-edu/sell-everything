09bffa0e2762f78c64d8d26101d5628fa060f28f
wooyoung <souljit2@gmail.com>
2021-04-29 23:48:51 +0900

    feat: MemberMapper에 signIn 메서드 추가
    MemberMapper.xml의 signIn과 매칭되는 signIn 메서드 추가

--- src/main/java/com/souljit2/selleverything/member/MemberMapper.java
+++ src/main/java/com/souljit2/selleverything/member/MemberMapper.java
@@ -4,5 +4,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberMapper {
-    public int signUp(MemberDTO memberDTO);
+    public void signUp(MemberDTO memberDTO);
+    public MemberDTO signIn(SignInRequestDTO signInRequestDTO);
 }
