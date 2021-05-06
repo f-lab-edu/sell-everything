27fc228fa5023154c1f4ac95b7f5c177137afbb8
wooyoung <souljit2@gmail.com>
2021-05-06 22:54:16 +0900

    refactor: MemberController의 null 분기 수정
    Service단에서 로그인 실패시 Exception을 발생시켜 Exception Handler로
    로그인 실패 처리 위임

--- src/main/java/com/souljit2/selleverything/member/MemberController.java
+++ src/main/java/com/souljit2/selleverything/member/MemberController.java
@@ -1,8 +1,8 @@
 package com.souljit2.selleverything.member;
 
+import com.souljit2.selleverything.utils.Responses;
+import com.souljit2.selleverything.utils.SessionUtils;
 import lombok.AllArgsConstructor;
-import org.springframework.dao.DuplicateKeyException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,22 +15,20 @@ import javax.validation.Valid;
 @RequestMapping("/apis/auth")
 public class MemberController {
 
-    private MemberServiceImpl memberService; /* @AllArgsConstructor에 의해 주입 */
+    private MemberServiceImpl memberService;
+
     @PostMapping("/signUp")
     public ResponseEntity signUp(@RequestBody @Valid MemberDTO memberDTO) throws Exception {
-        try {
-            memberService.signUp(memberDTO);
-            return new ResponseEntity(HttpStatus.CREATED);
-        } catch (DuplicateKeyException e){
-            return new ResponseEntity(HttpStatus.CONFLICT);
-        }
+        memberService.signUp(memberDTO);
+        return Responses.CREATED;
     }
 
     @PostMapping("/signIn")
-    public ResponseEntity<MemberDTO> signIn(@RequestBody @Valid SignInRequestDTO signInRequestDTO, HttpSession session) throws Exception  {
+    public ResponseEntity<MemberDTO> signIn(
+            @RequestBody @Valid SignInRequestDTO signInRequestDTO,
+            HttpSession session) throws Exception  {
         MemberDTO memberInfoDTO = memberService.signIn(signInRequestDTO);
-        session.setAttribute("member", memberInfoDTO);
-        if (memberInfoDTO != null) return new ResponseEntity<MemberDTO>(memberInfoDTO, HttpStatus.OK);
-        else return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        SessionUtils.setMemberSession(session, memberInfoDTO.getId());
+        return Responses.OK;
     }
 }
