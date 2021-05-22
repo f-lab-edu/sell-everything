d1a37f0b27b0b8a74e3df5219c58d46fe7b06147
wooyoung <souljit2@gmail.com>
2021-05-22 23:31:02 +0900

    feat: DB category enum 정보를 담고 있는 HashMap 상수 선언
    DB에 enum으로 정의된 column의 값들만을 요청 값으로 받기 위해
    enum값에 해당하는 String을 가지는 categories 선언

---
+++ src/main/java/com/souljit2/selleverything/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.souljit2.selleverything.utils;
+
+import java.util.HashMap;
+
+public final class Constants {
+
+    public static final HashMap<String, Boolean> categories = new HashMap() {{
+        put("CLOTHES", true);
+        put("INSTRUMENTS", true);
+        put("VEHICLES", true);
+        put("PHONES", true);
+        put("ACCESSORIES", true);
+        put("PROPS", true);
+        put("ETCS", true);
+    }};
+
+}
