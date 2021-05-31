48c5caaab235afdefcd9f8662137cae8e292fa6c
wooyoung <souljit2@gmail.com>
2021-05-31 22:57:08 +0900

    refactor: MemberDTO의 setter 제거
    Request로 전달되는 MemberDTO의 속성을 변경할 수 없도록 setter를
    제거하였습니다

--- src/main/java/com/souljit2/selleverything/model/MemberDTO.java
+++ src/main/java/com/souljit2/selleverything/model/MemberDTO.java
@@ -9,33 +9,39 @@ import java.time.LocalDateTime;
 
 @Getter
 @ToString
-@EqualsAndHashCode
+@AllArgsConstructor
 public class MemberDTO {
 
     @NonNull
     private int id;
+
     @NonNull
     @NotBlank(message = "ID를 입력해주세요.")
     private String memberId;
+
     @Pattern(regexp = "[a-zA-Z1-9]{6,12}", message = "비밀번호는 영어와 숫자가 포함된 6~12자리 문자를 입력해주세요.")
     private String memberPassword;
+
     @NonNull
     @NotBlank(message = "이름을 입력해주세요.")
     private String memberName;
+
     @NonNull
     @NotBlank(message = "닉네임을 입력해주세요.")
     private String memberNickname;
+
     @NonNull
     @NotBlank(message = "휴대폰 번호를 입력해주세요.")
     private String memberPhone;
+
     @AssertTrue
     private Boolean membershipAgreementYn;      /* 개인정보 활용 약관 동의. 필수*/
+
     @NonNull
     private Boolean optionalInfoAgreementYn;    /* 선택정보 활용 약관 동의. 선택 */
+
     private LocalDateTime createdAt;
+
     private LocalDateTime updatedAt;
 
-    public void setMemberPassword(String memberPassword) {
-        this.memberPassword = memberPassword;
-    }
 }
