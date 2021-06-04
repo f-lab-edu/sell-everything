ffa3f70348e30e9ec18d5df7f10113e4c023f51f
wooyoung <souljit2@gmail.com>
2021-06-05 01:52:10 +0900

    feat: PostController.java 추가
    PostController를 추가하고, Post에 대한 get 요청을 처리하는 핸들러
    메서드를 추가하였습니다

---
+++ src/main/java/com/souljit2/selleverything/post/PostController.java
@@ -0,0 +1,20 @@
+package com.souljit2.selleverything.post;
+
+import com.souljit2.selleverything.model.PostDTO;
+import com.souljit2.selleverything.service.PostService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/apis/post")
+public class PostController {
+
+    PostService postService;
+
+    @GetMapping("/getPostDetail/{id}")
+    public PostDTO getPostById(@PathVariable("id") int id) {
+        return postService.getPostById(id);
+    }
+
+}
