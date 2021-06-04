e4640a1843c240d1bdc23ec920163637078e0b8f
wooyoung <souljit2@gmail.com>
2021-06-05 03:04:38 +0900

    feat: 세션 검증을 위한 SessionCheckInterceptor, SessionExceptionHandlers 구현
    세션 인증이 필요한 요청시 세션 인증을 위한 SessionCheckInterceptor를 구현
    세션 인증 실패시 HttpSessionRequiredException을 발생시키고, 그에 대한 handler를 구현

--- src/main/java/com/souljit2/selleverything/interceptors/SessionCheckInterceptor.java
+++ src/main/java/com/souljit2/selleverything/interceptors/SessionCheckInterceptor.java
@@ -1,6 +1,7 @@
 package com.souljit2.selleverything.interceptors;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.HttpSessionRequiredException;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,9 +14,16 @@ public class SessionCheckInterceptor implements HandlerInterceptor {
     private static final String member = "member";
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) throws Exception {
         HttpSession session = request.getSession();
         Integer id = (Integer) session.getAttribute(member);
+        if (id == null) {
+            throw new HttpSessionRequiredException(
+                "There is no session information in the request header. "
+                    + "Use of that feature requires a session."
+            );
+        }
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
