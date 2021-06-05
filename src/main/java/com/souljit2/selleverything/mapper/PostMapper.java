17be05fbe94ff0d57a76a6f1c23e01787877ee15
wooyoung <souljit2@gmail.com>
2021-06-05 17:10:37 +0900

    feat: Post 다중 데이터 load용 Mapper 구현
    여러개의 Post를 가져오기 위한 Mapper를 구현하였습니다.

--- src/main/java/com/souljit2/selleverything/mapper/PostMapper.java
+++ src/main/java/com/souljit2/selleverything/mapper/PostMapper.java
@@ -3,9 +3,14 @@ package com.souljit2.selleverything.mapper;
 import com.souljit2.selleverything.model.PostDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+import java.util.Map;
+
 @Mapper
 public interface PostMapper {
 
     public PostDTO getPostById(int id);
+
+    public List<PostDTO> getPosts(Map<String, String> columnConditions);
 
 }
