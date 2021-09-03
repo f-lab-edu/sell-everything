fb318982438bc6fecf2eb8e3ddf6401ace078df3
wooyoung <souljit2@gmail.com>
2021-09-03 17:23:03 +0900

    fix: 좋아요 요청 성공시 Response Status 변경

--- src/main/java/com/souljit2/selleverything/domain/like/controller/LikeController.java
+++ src/main/java/com/souljit2/selleverything/domain/like/controller/LikeController.java
@@ -2,6 +2,7 @@ package com.souljit2.selleverything.domain.like.controller;
 
 import com.souljit2.selleverything.domain.like.service.LikeService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,11 +13,13 @@ public class LikeController {
     LikeService likeService;
 
     @PostMapping("/{postId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void likePost(@PathVariable int postId) {
         likeService.likePost(postId);
     }
 
     @DeleteMapping("/{postId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void unlikePost(@PathVariable int postId) {
         likeService.unlikePost(postId);
     }
