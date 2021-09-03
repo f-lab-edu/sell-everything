e4629717adf21dcd1d7d29bece27713e28efe05c
wooyoung <souljit2@gmail.com>
2021-09-03 16:38:52 +0900

    feat: like DB의 스키마 변경에 따라 LikeDTO 데이터 구조를 변경하였음

--- src/main/java/com/souljit2/selleverything/domain/like/model/LikeDTO.java
+++ src/main/java/com/souljit2/selleverything/domain/like/model/LikeDTO.java
@@ -2,6 +2,7 @@ package com.souljit2.selleverything.domain.like.model;
 
 import java.time.LocalDateTime;
 import javax.validation.constraints.Min;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NonNull;
@@ -14,11 +15,18 @@ public class LikeDTO {
 
     @NonNull
     @Min(1)
+    private int likeId;
+
+    @NonNull
+    @Min(1)
     private int memberIdFk;
 
     @NonNull
     @Min(1)
     private int postIdFk;
+
+    @NonNull
+    private boolean likeYn;
 
     private LocalDateTime createdAt;
 
