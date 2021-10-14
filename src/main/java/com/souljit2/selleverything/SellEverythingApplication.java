8d0a79ff1034a1838c420a69c1d8591c7d612ee4
wooyoung <souljit2@gmail.com>
2021-10-14 15:15:44 +0900

    fix: @EnableTransactionManagement 설정 누락하여 수정

--- src/main/java/com/souljit2/selleverything/SellEverythingApplication.java
+++ src/main/java/com/souljit2/selleverything/SellEverythingApplication.java
@@ -3,11 +3,12 @@ package com.souljit2.selleverything;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableCaching
+@EnableTransactionManagement
 @SpringBootApplication
 public class SellEverythingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SellEverythingApplication.class, args);
     }
